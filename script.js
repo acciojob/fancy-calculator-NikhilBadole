20e70c305975f8156c646ad29f0221e4059befaa
NikhilBadole <120246851+NikhilBadole@users.noreply.github.com>
2023-12-18 16:42:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let val1 = prompt("Enter");
+let first =  parseInt(val1) * 2;
+let val2 = prompt("Enter");
+let second = parseInt(val2);
+let result = first+second;
+alert('The final result is: ' + result);
+
